7076acb5cc293512ed5d70565c465eaea09c825b
Silvestro26 <50291487+Silvestro26@users.noreply.github.com>
2026-02-08 14:48:32 +0100

    Add sst/core package __init__.py with imports

---
+++ sst/core/__init__.py
@@ -0,0 +1,5 @@
+# SST Core Module
+from sst.core.claim import Claim
+from sst.core.classification import Classification
+from sst.core.self_test import SelfTest
+from sst.core.protocol import SSTProtocol
